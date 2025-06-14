ba0d792dec486820bde3e961040d95737eb6b971
cargo.king <ncc0706@gmail.com>
2025-06-14 23:01:31 +0800

    是否上传 JavaSourceJar,JavaDocJar

--- src/main/java/io/arukas/ArukasUtils.java
+++ src/main/java/io/arukas/ArukasUtils.java
@@ -4,6 +4,12 @@ import org.apache.commons.codec.digest.DigestUtils;
 
 public class ArukasUtils {
 
+    /**
+     * Generates an MD5 hash of the given data.
+     *
+     * @param data The input string to hash.
+     * @return The MD5 hash of the input string, or null if the input is null.
+     */
     public static String md5(String data) {
         if (data == null) return null;
         return DigestUtils.md5Hex(data);
